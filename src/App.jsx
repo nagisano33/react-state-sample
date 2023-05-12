e37f298120b186b5eb840ecbfcf9ba9d1907c4f0
nagisano33 <vita.stars2@gmail.com>
2023-05-13 02:48:12 +0900

    SampleBox を表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { SampleBox } from "./components/SampleBox";
 
 export const App = () => {
-  return <div className="App">aaa</div>;
+  return (
+    <div className="App">
+      <SampleBox />
+    </div>
+  );
 };
 
 // export default App;
